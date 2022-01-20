d9a9244e1ec8101655ec155e80a7a7ed9b758c56
mark burdett <mfburdett@gmail.com>
2022-01-20 12:29:12 -0800

    Handle edge cases with config out of sync or hardcoded

--- src/EventSubscriber/FileHashConfigSubscriber.php
+++ src/EventSubscriber/FileHashConfigSubscriber.php
@@ -5,7 +5,6 @@ namespace Drupal\filehash\EventSubscriber;
 use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Drupal\Core\Config\ConfigCrudEvent;
 use Drupal\Core\Config\ConfigEvents;
-use Drupal\Core\Database\Connection;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -22,13 +21,6 @@ class FileHashConfigSubscriber implements EventSubscriberInterface {
   protected $cacheTagsInvalidator;
 
   /**
-   * The database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $connection;
-
-  /**
    * The module handler.
    *
    * @var \Drupal\Core\Extension\ModuleHandlerInterface
@@ -40,14 +32,11 @@ class FileHashConfigSubscriber implements EventSubscriberInterface {
    *
    * @param \Drupal\Core\Cache\CacheTagsInvalidatorInterface $cache_tags_invalidator
    *   The cache tags invalidator.
-   * @param \Drupal\Core\Database\Connection $connection
-   *   The database connection to use.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
    */
-  public function __construct(CacheTagsInvalidatorInterface $cache_tags_invalidator, Connection $connection, ModuleHandlerInterface $module_handler) {
+  public function __construct(CacheTagsInvalidatorInterface $cache_tags_invalidator, ModuleHandlerInterface $module_handler) {
     $this->cacheTagsInvalidator = $cache_tags_invalidator;
-    $this->connection = $connection;
     $this->moduleHandler = $module_handler;
   }
 
@@ -61,23 +50,7 @@ class FileHashConfigSubscriber implements EventSubscriberInterface {
     if ($event->getConfig()->getName() !== 'filehash.settings' || !$event->isChanged('algos')) {
       return;
     }
-    $schema = $this->connection->schema();
-    foreach (filehash_algos() as $column => $algo) {
-      $spec['fields'] = [
-        $column => [
-          'description' => "The $algo hash for this file.",
-          'type' => 'varchar_ascii',
-          'length' => strlen(hash($algo, '')),
-          'not null' => FALSE,
-        ],
-      ];
-      if (!$schema->fieldExists('filehash', $column)) {
-        $schema->addField('filehash', $column, $spec['fields'][$column]);
-      }
-      if (!$schema->indexExists('filehash', "{$column}_idx")) {
-        $schema->addIndex('filehash', "{$column}_idx", [$column], $spec);
-      }
-    }
+    filehash_add_columns();
     // Invalidate the views data cache if configured algorithms were modified.
     if ($this->moduleHandler->moduleExists('views')) {
       $this->cacheTagsInvalidator->invalidateTags(['views_data']);
