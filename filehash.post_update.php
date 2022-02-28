85baee20ac2993daaf581f8b3a9e2f340f041fd9
mark burdett <mfburdett@gmail.com>
2022-02-27 23:23:50 -0800

    Detect new hook

---
+++ filehash.post_update.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for File Hash module.
+ */
+
+/**
+ * Add new hook_entity_storage_load() implementation.
+ */
+function filehash_post_update_new_hook() {
+}
